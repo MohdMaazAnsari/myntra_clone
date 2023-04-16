4deaa5a60d58282cb29340fa2cf7d271895305c5
maaz <mmaaz7460@gmail.com>
2023-04-16 14:47:57 +0530

    push

--- myntra_clone/src/components/Navbar.jsx
+++ myntra_clone/src/components/Navbar.jsx
@@ -1,33 +1,46 @@
 import React from 'react'
 
-import{Box, Flex,HStack,Spacer,Input,Image} from "@chakra-ui/react"
+import{Box, Flex,HStack,Spacer,Input} from "@chakra-ui/react"
 import { AiOutlineSearch,AiOutlineUser,AiOutlineHeart } from "react-icons/ai";
 import { BiCartAlt } from "react-icons/bi";
+import {Nav,Center} from "../styles/Navbar_Style"
+
 
 export const Navbar = () => {
   return (
-   <Box>
+   <Box >
 
-    <Flex justifyContent={'space-evenly'} m={10}>
+    <Flex sx={Nav} justifyContent={'space-evenly'} m={10}>
 
-<HStack spacing="30" bg="blue.800" p="5">
-<Box h="40px" w="40px">
+<HStack spacing="30" bg="blue.300" p="5">
+<Box w="60px" >
 
-    <img  src="https://images.indianexpress.com/2021/01/myntra.png" w="100%" h="100%" alt="logo"/>
+    <img src="https://images.indianexpress.com/2021/01/myntra.png" w="100%"
+    h="40px" alt="logo"/>
 </Box>
 
-<Box >men</Box>
-<Box >women</Box>
-<Box>kids</Box>
-<Box>home & living</Box>
-<Box>beauty</Box>
+<Box>
+<Box >MEN</Box>
+<ul>
+    <li>a</li>
+    <li>b</li>
+    <li>c</li>
+    <li>d</li>
+    <li>e</li>
+</ul>
+
+</Box>
+<Box >WOMEN</Box>
+<Box>KIDS</Box>
+<Box>HOME & LIVING</Box>
+<Box>BEAUTY</Box>
 
 </HStack>
 <Spacer/>
 
 {/* ----------------- */}
-<HStack bg="blue.600" w="40%">
-<Box w="100%">
+<HStack bg="blue.300" w="40%">
+<Box w="100%" m="20px">
     <Input/>
     {/* searchbar */}
 </Box>
@@ -37,11 +50,17 @@ export const Navbar = () => {
 {/* ------------------ */}
 <Spacer/>
 
-<HStack spacing="30">
+<HStack  spacing="30">
     
-<Box><AiOutlineUser/></Box>
-<Box><AiOutlineHeart/></Box>
-<Box><BiCartAlt/></Box>
+<Box sx={Center}><AiOutlineUser/>
+<span>Profile</span>
+</Box>
+<Box><AiOutlineHeart/>
+<span>Wishlist</span>
+</Box>
+<Box><BiCartAlt/>
+<span>Cart</span>
+</Box>
 
 </HStack>
     </Flex>
---
+++ myntra_clone/src/styles/Navbar_Style.jsx
@@ -0,0 +1,12 @@
+export const Nav={
+//    fontSize:"50px" 
+    alignItems:"center"
+}
+
+export const Center={
+   margin:"auto",
+
+    textAlign:"center",
+   alignItems:"center",
+   border:"1px solid red"
+}
